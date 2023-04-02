2f402655dd98b7aa8e30e6e9ffe2e4cae708c95c
Rohit Prasad <rohitpd8101@gmail.com>
2023-04-02 11:12:13 +0530

    api link update

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance= axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://alumni-backend-w4yl.onrender.com'
 });
 
 export default instance;
